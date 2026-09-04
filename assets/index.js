518e143cbe5f96cd7e65eee88a4c0055e29b04d5
Farguts <torusoportewa@gmail.com>
2026-09-04 14:32:07 -0300

    Añadido: nuevo `import` de `stickerEmoji` para crear stickers

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,7 @@
+import stickerEmoji from './src/_box/sticker/stickerEmoji.js';
 import { images, img } from './functions/_mod/avatars.js';
 import { formats } from './functions/_mod/formats.js';
 
 export {
-images, img, formats
+stickerEmoji, aimages, img, formats
 }
