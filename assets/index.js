36f21ff36f1078f197cef52b9ee70e407d80f310
Farguts <torusoportewa@gmail.com>
2026-09-05 12:06:15 -0300

    Añadir nuevo import al export fix #4

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,9 @@
 import stickerEmoji from './src/_box/sticker/stickerEmoji.js';
 import { images, img } from './functions/_mod/avatars.js';
 import formats from './functions/_mod/formats.js';
+import { parseFlags, applyTransformation, isImage, FLAG_MAP, SUPPORTED as sharpSupported } from './src/_box/converter/sharp/index.js';
 
 export {
-stickerEmoji, images, img, formats
+stickerEmoji, images, img, formats, parseFlags, applyTransformation, isImage,
+FLAG_MAP, sharpSupported
 }
