dc20fa198b5acbfced700a62abc663c452dd6f1a
Farguts <torusoportewa@gmail.com>
2026-09-05 14:33:02 -0300

    Actualizar envío de backup #2

--- plugins/owner/backup.js
+++ plugins/owner/backup.js
@@ -1,5 +1,5 @@
 import fsPromise from 'fs/promises'
-import { structure } from '../../core/models.js'
+import { models } from '../../core/models.js'
 
 export const run = {
    usage: ['backup'],
@@ -7,8 +7,8 @@ export const run = {
    async: async (m, { client, Config, setting, system, Utils }) => {
       try {
          await client.sendReact(m.chat, setting.timeLoad, m.key)
-         const data = await system.proxy.backup(structure, Config.database)
-         const now = new Intl.DateTimeFormat('en-CA', { timeZone: process.env.TZ, hour12: false, year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' }).format(new Date()).replace(', ', '_').replace(/:/g, '-')
+         const data = await system.proxy.backup(models.structure, Config.database)
+         const now = new Intl.DateTimeFormat('es-AR', { timeZone: process.env.TZ, hour12: false, year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' }).format(new Date()).replace(', ', '_').replace(/:/g, '-')
          const filename = `${Config.database}-${now}.json`
          await fsPromise.writeFile(filename, data, 'utf-8')
          const buffer = await fsPromise.readFile(filename)
