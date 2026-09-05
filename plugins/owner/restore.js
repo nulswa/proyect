46536b85093f17eebb159e4e22f89e51b1d22e92
Farguts <torusoportewa@gmail.com>
2026-09-05 14:34:18 -0300

    Actualizar actualización del bot #2

--- plugins/owner/restore.js
+++ plugins/owner/restore.js
@@ -1,18 +1,18 @@
 import fsPromise from 'fs/promises'
-import { structure } from '../../core/models.js'
+import { models } from '../../core/models.js'
 
 export const run = {
    usage: ['restore'],
    category: 'owner',
-   async: async (m, { client, Config, system, Utils, setting }) => {
+   async: async (m, { client, Config, system, setting, Utils }) => {
       try {
          if (m.quoted && /document/.test(m.quoted.mtype) && /json/.test(m.quoted.fileName)) {
-            await client.sendReact(m.chat, '⏰', m.key)
+            await client.sendReact(m.chat, setting.timeLoad, m.key)
             const fn = await Utils.getFile(await m.quoted.download())
-            if (!fn.status) return client.reply(m.chat, `${setting.emoji}  El archivo no se puede descargar.`, m)
+            if (!fn.status) return client.reply(m.chat, `${setting.emoji}  El archivo no se pudo descargar.`, m)
             const data = await fsPromise.readFile(fn.file, 'utf-8')
-            await system.proxy.restore(structure, data, Config.database)
-            client.reply(m.chat, `${setting.emoji2}  La base de datos se restauro con exito.`, m)
+            await system.proxy.restore(models.structure, data, Config.database)
+            client.reply(m.chat, `${setting.emoji2}  La base de datos se restableció correctamente.  :)`, m)
          } else client.reply(m.chat, `${setting.emoji}  Responda al archivo y use este comando.`, m)
       } catch (e) {
          return client.reply(m.chat, Utils.jsonFormat(e), m)
