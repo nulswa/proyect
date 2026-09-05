4bb67c0f775ab468529bb735fcb2925e3d33f5ff
Farguts <torusoportewa@gmail.com>
2026-09-05 09:44:39 -0300

    Corregir export `aimages` por `images` fix #1

--- assets/index.js
+++ assets/index.js
@@ -3,5 +3,5 @@ import { images, img } from './functions/_mod/avatars.js';
 import { formats } from './functions/_mod/formats.js';
 
 export {
-stickerEmoji, aimages, img, formats
+stickerEmoji, images, img, formats
 }
