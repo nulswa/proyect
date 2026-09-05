c7b8da522a0dc4e4eb369c943e36c6d730dc0366
Farguts <torusoportewa@gmail.com>
2026-09-05 12:12:36 -0300

    Update v2.js

--- assets/src/_box/converter/sharp/v2.js
+++ assets/src/_box/converter/sharp/v2.js
@@ -1,1 +1,12 @@
+export const SUPPORTED = {
+  shapes: [],
+  effects: [],
+}
 
+export function parseFlags(args = []) {
+  return { shapes: [], effects: [], cleanArgs: args, hasFlags: false }
+}
+
+export async function applyTransformation(buffer, shapes = [], effects = []) {
+  throw new Error('v2: no implementado todavía')
+}
