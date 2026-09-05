bb6c8810922a3086d3b806651150c7eea82e5f15
Farguts <torusoportewa@gmail.com>
2026-09-05 09:39:28 -0300

    Nuevo comando [stickers meta] para crear stickers al estilo meta

---
+++ plugins/stickers/sticker-meta.js
@@ -0,0 +1,32 @@
+export const run = {
+   usage: ['sticker-lock', 'sticker-prem', 'sticker-ai'],
+   hidden: ['s-lock', 's-prem', 's-ai'],
+   category: 'stickers',
+   async: async (m, { client, command, setting, Utils }) => {
+      try {
+         const q = m.quoted ? m.quoted : m
+         const mime = (q.msg || q).mimetype || ''
+         if (/image\/(jpe?g|png|webp)/.test(mime)) {
+            const buffer = await q.download()
+            if (!buffer) return client.reply(m.chat, `${setting.emoji}  Responda a un vídeo o imagen.`, m)
+            await client.sendReact(m.chat, setting.timeLoad, m.key)
+            client.sendSticker(m.chat, buffer, m, {
+               packname: setting.sk_pack,
+               author: setting.sk_author, ...(command === 'stclock' ? { lock: true } : command === 'stcprem' ? { premium: true } : command === 'stcai' ? { meta: true } : {} )
+            })
+         } else if (/video/.test(mime)) {
+            if ((q.msg || q).seconds > 10) return client.reply(m.chat, `${setting.emoji}  El vídeo no debe durar más de *10* segundos.`, m)
+            const buffer = await q.download()
+            if (!buffer) return client.reply(m.chat, `${setting.emoji}  Responda a un vídeo o imagen.`, m)
+            await client.sendReact(m.chat, setting.timeLoad, m.key)
+            client.sendSticker(m.chat, buffer, m, {
+               packname: setting.sk_pack,
+               author: setting.sk_author, ...(command === 'stclock' ? { lock: true } : command === 'stcprem' ? { premium: true } : command === 'stcai' ? { meta: true } : {} )
+            })
+         } else client.reply(m.chat, `${setting.emoji2}  Responda a un vídeo o imagen.`, m)
+      } catch (e) {
+         client.reply(m.chat, Utils.jsonFormat(e), m)
+      }
+   },
+   error: false
+}
